df86997bbe438bbf3622473d0af485a380e8aad8
notSaqlain <iamnotsaqlain@gmail.com>
2024-12-05 08:51:32 +0100

    Add getters and setters

--- Node.java
+++ Node.java
@@ -1,3 +1,28 @@
-public class Node {
+public class Node<T> {
+    T value;
+    Node<T> next;
+
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    
     
 }
