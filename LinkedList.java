5dab3363c76eb6b55ed20fd4de8f91e2a8c9a58a
notSaqlain <iamnotsaqlain@gmail.com>
2024-12-05 09:19:45 +0100

    Implement generic LinkedList class with addHead method

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,19 @@
-public class LinkedList {
-    
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void addHead(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+    }
 }
