7df6d49e82183bb675745f46559c5a672f44d3e4
notSaqlain <iamnotsaqlain@gmail.com>
2024-12-07 21:33:46 +0100

    Add constructor and toString() method to JavaLista class for array initialization and representation

--- JavaLista.java
+++ JavaLista.java
@@ -1,3 +1,25 @@
 public class JavaLista {
+    // public JavaLista(int[] array) {
+    //     LinkedList<Integer> list = new LinkedList<>();
+    //     for (int i = 0; i < array.length; i++) {
+    //         list.addHead(array[i]);
+    //     }
+    //     System.out.println(list);
+    // }
+
+    private LinkedList<Integer> list;
+
+    public JavaLista(int[] array) {
+        list = new LinkedList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.addHead(array[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "JavaLista [list=" + list + "]";
+    }
+
     
 }
