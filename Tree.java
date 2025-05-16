f75b4da90370442d3c3e1174a88a79428b809677
notSaqlain <117275944+notSaqlain@users.noreply.github.com>
2025-05-16 08:42:51 +0000

    Aggiungi la classe Tree per la gestione di un albero binario

---
+++ Tree.java
@@ -0,0 +1,34 @@
+public class Tree <T extends Comparable<T>> {
+    private TreeNode<T> root;
+
+    public Tree() {
+        this.root = null;   
+    }
+
+    public void add(T value) {
+        TreeNode<T> newNode = new TreeNode<>(value);
+        if (root == null) {
+            root = newNode;
+        } else {
+            while(true) {
+                if (value.compareTo(root.getValue()) < 0) {
+                    if (root.getLeft() == null) {
+                        root.setLeft(newNode);
+                        break;
+                    } else {
+                        root = root.getLeft();
+                    }
+                } else if (value.compareTo(root.getValue()) > 0) {
+                    if (root.getRight() == null) {
+                        root.setRight(newNode);
+                        break;
+                    } else {
+                        root = root.getRight();
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
